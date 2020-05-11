ef702a44912a5050fad54bc6a8eaa898f3c0b48c
ne1410s <pgj646@gmail.com>
2020-05-11 21:39:48 +0100

    Retain control over templating!

--- src/menu/menu.ts
+++ src/menu/menu.ts
@@ -1,8 +1,9 @@
-import { q } from '@ne1410s/dom';
+import { q, ChainSource } from '@ne1410s/dom';
 import { CustomElementBase } from '@ne1410s/cust-elems';
 
 import markupUrl from './menu.html';
 import stylesUrl from './menu.css';
+import { QuickParam } from '@ne1410s/dom/dist/models';
 
 export class NeMenu extends CustomElementBase {
 
@@ -11,7 +12,7 @@ export class NeMenu extends CustomElementBase {
   private readonly top: HTMLUListElement;
 
   constructor() {
-    super(stylesUrl, markupUrl, 'open');
+    super(stylesUrl, markupUrl);
     this.top = this.root.querySelector('ul');
   }
 
@@ -29,15 +30,19 @@ export class NeMenu extends CustomElementBase {
         break;
 
       case 'open':
+        
+        // close all menus
+        const doc = this.parentElement.getRootNode() as Document;
+        doc.querySelectorAll('ne14-menu').forEach((m: NeMenu) => m.close());
+
+        // style this one as open
         this.top.classList.add('open');
         break;
     }
   }
+
   connectedCallback() {
-
-    console.log(this.root.querySelector('slot').assignedNodes());
-    console.log(this.shadowRoot.querySelector('slot').assignedNodes());
-
+    setTimeout(() => this.reload());
     q(this.parentNode).on('contextmenu', (e: MouseEvent) => this.onParentContext(e));
     q(this).on('mousedown wheel', e => e.stopPropagation());
     q(window).on('mousedown resize wheel', () => this.close());
@@ -48,13 +53,20 @@ export class NeMenu extends CustomElementBase {
     console.warn('TODO: prevent event handler build-up');
   }
 
-  open() {
+  /** Opens the menu.  */
+  open(): void {
     this.setAttribute('open', '');
   }
 
-  close() {
+  /** Closes the menu. */
+  close(): void {
     this.removeAttribute('open');
     this.top.classList.remove('open');
+  }
+
+  /** Reloads active contents based on client dom. */
+  reload(): void {
+    q(this.top).empty().append(...this.walk(this));
   }
 
   private onParentContext(event: MouseEvent) {
@@ -65,15 +77,34 @@ export class NeMenu extends CustomElementBase {
     event.preventDefault();
     event.stopPropagation();
 
-    // close all windows
-    const doc = this.parentElement.getRootNode() as Document;
-    doc.querySelectorAll('ne14-menu').forEach((m: NeMenu) => m.close());
-
     // update position
     this.top.style.left = `${event.clientX}px`;
     this.top.style.top = `${event.clientY}px`;
 
-    // open this one
+    // open
     this.open();
+  }
+
+  private walk(ul: ParentNode): ChainSource[] {
+    return Array
+      .from(ul.children)
+      .filter(c => c.textContent?.trim())
+      .map(c => {
+
+        // todo we can propagate target=blank, if we use <a> wrapper round
+        // li items. (Could use javascript:void(0) or simply omit else)
+
+        const param = { tag: 'li', text: c.textContent, attr: {} } as QuickParam;
+        const onclick = c.hasAttribute('onclick')
+          ? c.getAttribute('onclick')
+          : c.hasAttribute('href')
+            ? `window.location.href="${c.getAttribute('href')}"`
+            : null;
+
+        if (onclick) param.attr['onclick'] = onclick;
+        else param.attr['class'] = 'disabled';
+
+        return param;
+      });
   }
 }
