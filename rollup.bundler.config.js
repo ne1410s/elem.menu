0c92d7e7fbc150d50bd97593b9241e793775acff
ne1410s <pgj646@gmail.com>
2020-06-08 20:50:20 +0100

    Deps Audit June '20

--- rollup.bundler.config.js
+++ rollup.bundler.config.js
@@ -1,4 +1,3 @@
-import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import json from '@rollup/plugin-json';
 import url from '@rollup/plugin-url';
@@ -17,7 +16,6 @@ export default {
   ],
   plugins: [
     typescript(),
-    terser({ include: ['*.cjs.min.js', '*.esm.min.js'] }),
     json(),
     url({ include: ['src/**/*.css', 'src/**/*.html'] })
   ]
